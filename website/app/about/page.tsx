44f4741ce27860d232cc83f3f9747b3b58cbcc5e
willem.serruys <willem.serruys@symphonyai.com>
2025-05-24 14:19:27 +0200

    Updated authors page

--- website/app/about/page.tsx
+++ website/app/about/page.tsx
@@ -3,21 +3,37 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
+import { Author } from 'next/dist/lib/metadata/types/metadata-types'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
 	return (
 		<>
-			<h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-				About
-			</h1>
-			{allAuthors.map(a =>
-				<AuthorLayout key={a._id} content={coreContent(a)}>
-					<MDXLayoutRenderer code={a.body.code} />
-				</AuthorLayout>
-			)
-			}
+			<div className="divide-y divide-gray-200 dark:divide-gray-700">
+				<div className="space-y-2 pt-6 pb-8 md:space-y-5">
+					<h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
+						About
+					</h1>
+					{allAuthors.sort(sortAuthorsByName).map(a =>
+						<AuthorLayout key={a._id} content={coreContent(a)}>
+							<MDXLayoutRenderer code={a.body.code} />
+						</AuthorLayout>
+					)
+					}
+				</div>
+			</div>
 		</>
 	)
 }
+
+function sortAuthorsByName (a: Author, b: Author) {
+	if (a.name! < b.name!) {
+		return -1;
+	}
+	if (a.name! > b.name!) {
+		return 1;
+	}
+	return 0;
+}
+
