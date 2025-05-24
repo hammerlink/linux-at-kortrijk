b5009d8d90233e53618f86b7c4bcc519aee1b3fb
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-05-24 17:08:12 +0200

    add analytics, favicon & remove styleswitcher

--- website/data/siteMetadata.js
+++ website/data/siteMetadata.js
@@ -5,14 +5,15 @@ const siteMetadata = {
   headerTitle: 'Linux @ Kortrijk',
   description: 'Linux @ Kortrijk helpt je met Linux en Open Source software.',
   language: 'nl-be',
+  locale: 'nl-BE',
   theme: 'light', // system, dark or light
   siteUrl: 'https://linux-at-kortrijk.be',
   siteRepo: 'https://github.com/hammerlink/linux-at-kortrijk',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  // mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'hendrik.hamerlinck@hammernet.be',
   github: 'https://github.com',
+  // mastodon: 'https://mastodon.social/@mastodonuser',
   // x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
   // facebook: 'https://facebook.com',
@@ -22,20 +23,19 @@ const siteMetadata = {
   // instagram: 'https://www.instagram.com',
   // medium: 'https://medium.com',
   // bluesky: 'https://bsky.app/',
-  locale: 'nl-BE',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    // umamiAnalytics: {
-    //   // We use an env variable for this site to avoid other users cloning our analytics ID
-    //   umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    //   // You may also need to overwrite the script if you're storing data in the US - ex:
-    //   // src: 'https://us.umami.is/script.js'
-    //   // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
-    // },
+    umamiAnalytics: {
+      // We use an env variable for this site to avoid other users cloning our analytics ID
+      umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+      // You may also need to overwrite the script if you're storing data in the US - ex:
+      // src: 'https://us.umami.is/script.js'
+      // Remember to add 'us.umami.is' in `next.config.js` as a permitted domain for the CSP
+    },
     // plausibleAnalytics: {
     //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     // If you are hosting your own Plausible.
@@ -49,11 +49,11 @@ const siteMetadata = {
     //   googleAnalyticsId: '', // e.g. G-XXXXXXX
     // },
   },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
-    // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
-  },
+  // newsletter: {
+  //   // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
+  //   // Please add your .env file and modify it according to your selection
+  //   provider: 'buttondown',
+  // },
   comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
