caca05b787ce4c1bde58d6c48ff72c7f3072ffcf
willem.serruys <willem.serruys@symphonyai.com>
2025-05-24 14:15:21 +0200

    Updated authors page

--- website/app/about/page.tsx
+++ website/app/about/page.tsx
@@ -9,6 +9,9 @@ export const metadata = genPageMetadata({ title: 'About' })
 export default function Page() {
 	return (
 		<>
+			<h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
+				About
+			</h1>
 			{allAuthors.map(a =>
 				<AuthorLayout key={a._id} content={coreContent(a)}>
 					<MDXLayoutRenderer code={a.body.code} />
