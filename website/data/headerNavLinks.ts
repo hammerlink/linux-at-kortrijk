339136174976a2d676996932d7a29f72fd37604d
Hendrik Hamerlinck <hendrik.hamerlinck@hammernet.be>
2025-09-09 15:21:17 +0200

    add exercises

--- website/data/headerNavLinks.ts
+++ website/data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
+  { href: '/exercises', title: 'Oefeningen' },
   { href: '/events', title: 'Events' },
   { href: '/about', title: 'Over ons' },
 ]
